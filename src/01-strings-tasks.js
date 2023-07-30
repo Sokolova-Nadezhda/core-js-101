abfb0b7144f4d4709ce2aa8eb0bd89daa5899ae6
unknown <sokolova.nadezhda.31@gmail.com>
2023-07-30 16:10:33 +0300

    commit1

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -18,8 +18,20 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  if (value1 === '' && value2 === '') {
+    return '';
+  }
+
+  if (value1 === '') {
+    return value2;
+  }
+
+  if (value2 === '') {
+    return value1;
+  }
+
+  return value1 + value2;
 }
 
 
@@ -34,8 +46,8 @@ function concatenateStrings(/* value1, value2 */) {
  *   'b'     => 1
  *   ''      => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  return value.length;
 }
 
 /**
@@ -51,8 +63,8 @@ function getStringLength(/* value */) {
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -65,8 +77,11 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const startIndex = value.indexOf('Hello, ') + 7;
+  const endIndex = value.indexOf('!', startIndex);
+  const name = value.substring(startIndex, endIndex);
+  return name.trim();
 }
 
 
@@ -80,8 +95,8 @@ function extractNameFromTemplate(/* value */) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value.charAt(0);
 }
 
 /**
